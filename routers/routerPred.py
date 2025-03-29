ef0fb771917852729d9a1f630518679923ac5c31
AlexTaquez <alextaquez@unicomfacauca.edu.co>
2025-03-29 17:10:47 -0500

    com

---
+++ routers/routerPred.py
@@ -0,0 +1,33 @@
+import pickle
+from fastapi import APIRouter
+import numpy as np
+from interfaz import dataCultivo
+
+router = APIRouter()
+
+with open("svcPredict2.pkl","rb") as file:
+    model=pickle.load(file)
+
+
+@router.post("/cultivo")
+def patient(data: dataCultivo):
+    data = data.model_dump()
+    print("========================================")
+
+    N=data["N"]
+    P=data["P"]
+    K=data["K"]
+    temperature=data["temperature"]
+    humidity=data["humidity"]
+    ph=data["ph"]
+    rainfall=data["rainfall"]
+
+    xin = np.array([N,P,K,temperature,humidity,ph,rainfall]).reshape(1,7)
+
+    print(xin)
+    prediction = model.predict(xin)
+
+    print(prediction)
+
+    return {"patient":prediction[0]}
+
